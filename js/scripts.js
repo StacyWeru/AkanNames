85e0ce369174281aaa45da1b33d1ef338b9cd61d
StacyWeru <stacyweru@gmail.com>
2020-09-03 17:33:46 +0300

    Declare variables for the days othe week and akan names for both genders

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,12 @@ function output() {
     var cc = document.getElementById("year").value.slice(0,2);
     var gender = parseInt(document.getElementById("gender").value);
 
-    
+
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    var day = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
+    var akanDay = days[Math.floor(day)];
+
+
 }
