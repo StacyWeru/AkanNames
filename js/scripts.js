efdbad14afd8d7d0b20eb383858ff9069ea24c79
StacyWeru <stacyweru@gmail.com>
2020-09-03 17:24:40 +0300

    Declare the variables for the date,month,year and gender

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function output() {
+
+    var dd = document.getElementById("date").value;
+    var mm = document.getElementById("month").value;
+    var yy = document.getElementById("year").value.slice(2,4);
+    var cc = document.getElementById("year").value.slice(0,2);
+    var gender = parseInt(document.getElementById("gender").value);
+
+    
+}
